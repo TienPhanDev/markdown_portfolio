0b84d798dfa63ccee84f7a002010728b5c830118
tienphandev <tienphan.dev@gmail.com>
2021-07-27 02:14:02 -0400

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import "../styles/index.css"
 import PropTypes from 'prop-types';
-import Head from 'next/head';
 import { ThemeProvider } from '../components/themeContext';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-      </Head>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ export default class MyDocument extends Document {
       <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
